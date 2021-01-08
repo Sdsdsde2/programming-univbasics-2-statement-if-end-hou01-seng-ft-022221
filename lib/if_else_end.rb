e3e269a449642ab6d98d34c3818dab5b0fca3a63
Shane R <imspammails@gmail.com>
2021-01-08 07:16:48 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,13 @@
 # Write your solution here
+# Get the current time.
+my_current_time = Time.now
+
+# Get the current time in seconds.
+my_current_time = my_current_time.to_i
+
+# Check if the seconds end in an even or odd.
+if my_current_time % 2 == 0 
+  puts "Even!"
+else 
+  puts "Odd!"
+end 
